067a86f403d143d71f688db7f56e809aaa42100d
Lisa Hull <lisa@jchull.com>
2016-08-25 13:05:14 -0600

    dog and kitty button working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // renders html
+  res.render('index', {
+    title: 'Awesome Pet Page',
+    pet1: 'dog',
+    pet2: 'cat'
+  });
+});
+
+router.get('/:', function(req, res, next){
+  res.render('dog1', {
+
+  })
 });
 
 module.exports = router;
+
+
+
+
+// cat1 url http://i.giphy.com/vqPOUf6mzrSve.gif
+// cat2 url http://i.giphy.com/Vj5ZgHbXa3kWY.gif
+// cat3 url http://i.giphy.com/wv3pJQDUfpyY8.gif
+
+//dog1 url http://i.giphy.com/DgzJFvt6StyFi.gif
+// dog2 url http://i.giphy.com/DFmCDPP9tR92o.gif
+// dog3 url http://i.giphy.com/aFOtri4IywD7i.gif
